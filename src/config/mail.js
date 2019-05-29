396afef6bfcd2655322c6c8591a090c5db8b4151
thomasneo <thomaslossio@hotmail.com>
2019-05-28 22:08:10 -0300

    Intializing tests, clearing database before each test, testing password hash, authentication, middleware authentication, using factory girl + faker, mokc and delivery mails and, coverage report

---
+++ src/config/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+  auth: {
+    user: "asdasdsad",
+    pass: "asdasd"
+  },
+  port: 2525,
+  host: "smtp.mailtrap.io"
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 const routes = require("express").Router();
 
-const { User } = require("./app/models");
+const SessionController = require("./app/controllers/SessionController");
 
-routes.get("/", async (req, res) => {
-  const user = await User.create({
-    name: "Thomas",
-    email: "thomaslossio@hotmail.com",
-    password_hash: "123456"
-  });
+const authMiddleware = require("./app/middlewares/auth");
 
-  return res.json({ user });
+routes.post("/sessions", SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.get("/dashboard", (req, res) => {
+  return res.status(200).send();
 });
 
 module.exports = routes;
